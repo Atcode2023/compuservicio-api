e97b754fbadcbe0f3c5402ffc1701a05f739bb2a
Humberto Andueza <humbertoanduezaa@gmail.com>
2024-08-06 16:33:26 -0400

    changes

--- database/migrations/2024_05_29_231734_add_uuid_and_last_synced_at_to_all_tables.php
+++ database/migrations/2024_05_29_231734_add_uuid_and_last_synced_at_to_all_tables.php
@@ -13,46 +13,16 @@ class AddUuidAndLastSyncedAtToAllTables extends Migration
 
         foreach ($tables as $table) {
             $tableName = array_values((array)$table)[0];
-
-            // Ignorar la tabla de migraciones
             if ($tableName === 'migrations') {
                 continue;
             }
-
-            // Agregar columnas `uuid` y `last_synced_at` como `nullable`
             Schema::table($tableName, function (Blueprint $table) use ($tableName) {
                 if (!Schema::hasColumn($tableName, 'uuid')) {
-                    $table->uuid('uuid')->unique()->nullable();
+                    $table->uuid('uuid')->unique()->default(DB::raw('(UUID())'));
                 }
                 if (!Schema::hasColumn($tableName, 'last_synced_at')) {
                     $table->timestamp('last_synced_at')->nullable();
                 }
-            });
-        }
-
-        // Actualizar las filas existentes para establecer el valor de `uuid`
-        foreach ($tables as $table) {
-            $tableName = array_values((array)$table)[0];
-
-            // Ignorar la tabla de migraciones
-            if ($tableName === 'migrations') {
-                continue;
-            }
-
-            DB::table($tableName)->whereNull('uuid')->update(['uuid' => DB::raw('UUID()')]);
-        }
-
-        // Establecer la columna `uuid` como `NOT NULL`
-        foreach ($tables as $table) {
-            $tableName = array_values((array)$table)[0];
-
-            // Ignorar la tabla de migraciones
-            if ($tableName === 'migrations') {
-                continue;
-            }
-
-            Schema::table($tableName, function (Blueprint $table) {
-                $table->uuid('uuid')->nullable(false)->change();
             });
         }
     }
@@ -62,13 +32,11 @@ class AddUuidAndLastSyncedAtToAllTables extends Migration
         $tables = DB::select('SHOW TABLES');
 
         foreach ($tables as $table) {
-            $tableName = array_values((array)$table)[0];
 
-            // Ignorar la tabla de migraciones
+            $tableName = array_values((array)$table)[0];
             if ($tableName === 'migrations') {
                 continue;
             }
-
             Schema::table($tableName, function (Blueprint $table) {
                 $table->dropColumn(['uuid', 'last_synced_at']);
             });
